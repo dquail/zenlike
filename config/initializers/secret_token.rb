6d537f666673dc020134c9e19267cee233e4ebd6
David Quail <quail.david@gmail.com>
2013-02-14 13:40:44 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zenlike::Application.config.secret_token = 'f4ba0b9d17939c71e99f450980eac20caf4be81db17b2b79d62795efd46ae8eaa920080518bc236823c0a029abcca8a2d6b2c370c1bd8731d94c63f43878aee5'
