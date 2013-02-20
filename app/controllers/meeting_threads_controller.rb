1be779c381c1abfed179075dc0635cf8e5ec16cb
David Quail <quail.david@gmail.com>
2013-02-19 23:50:42 -0800

    Temp disable sending email

--- app/controllers/meeting_threads_controller.rb
+++ app/controllers/meeting_threads_controller.rb
@@ -86,6 +86,8 @@ class MeetingThreadsController < ApplicationController
   end
   
   def from_sendgrid
+    #temporarily returning so sendgrid stops spamming me
+    return
     logger.debug "Received request from sendgrid"
 
     #populate the meeting thread with the params    
