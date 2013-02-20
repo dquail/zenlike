1f8198ff0d95b1d014ff23732849840c1230360f
David Quail <quail.david@gmail.com>
2013-02-20 11:41:46 -0800

    Abort sendgrid

--- app/controllers/meeting_threads_controller.rb
+++ app/controllers/meeting_threads_controller.rb
@@ -86,8 +86,8 @@ class MeetingThreadsController < ApplicationController
   end
   
   def from_sendgrid
-    #render :json => { "message" => "OK" }, :status => 200
-    #return
+    render :json => { "message" => "OK" }, :status => 200
+    return
     logger.debug "Received request from sendgrid"
 
     #populate the meeting thread with the params    
