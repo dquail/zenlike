5ff133c707ad2b7c81f266550e82e588e96d6922
David Quail <quail.david@gmail.com>
2013-02-24 23:42:40 -0800

    Added constraint that start time must be before end time

--- app/models/calendar_guess.rb
+++ app/models/calendar_guess.rb
@@ -3,6 +3,7 @@ class CalendarGuess < ActiveRecord::Base
   attr_accessible :participants, :start_date_time, :end_date_time, :time_zone, :location, :description, :turker_id, :meeting_thread_id
   validates_presence_of :participants, :start_date_time, :end_date_time, :time_zone, :location, :turker_id, :meeting_thread_id
   validate :check_participants
+  validate :start_must_be_before_end_time
   
   belongs_to :turker, :class_name => "Turker", :foreign_key => 'turker_id'
   belongs_to :meeting_thread
@@ -14,4 +15,9 @@ class CalendarGuess < ActiveRecord::Base
       end
     end
   end  
+  
+  def start_must_be_before_end_time
+      errors.add(:start_time, "must be before end time") unless
+         self.start_date_time < self.end_date_time
+    end  
 end
