ca72e8220bfa8064ad5e73c87137704bb71a93e0
David Quail <quail.david@gmail.com>
2013-03-01 23:17:37 -0800

    Changed default host for actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Zenlike::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'http://www.zenlike.me' }  
+  config.action_mailer.default_url_options = { :host => "meetings.zenlike.me", :protocol => 'https' }  
 
   # Enable threaded mode
   # config.threadsafe!
