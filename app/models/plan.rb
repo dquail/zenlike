2ccbb22576eba1c2dc11c2237955412c0644f210
David Quail <quail.david@gmail.com>
2013-03-06 14:52:46 -0800

    attr_accessible for plans

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,6 @@
 class Plan < ActiveRecord::Base
   validates_presence_of :stripe_id
   validates_uniqueness_of :stripe_id
-    
+  attr_accessible :name, :stripe_id, :price, :credits  
   has_many :subscriptions
 end
