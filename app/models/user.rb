9d9ca4180ce56d907608975dfcb8900a5a46f58b
David Quail <quail.david@gmail.com>
2013-03-12 14:40:03 -0700

    Dont force user to confirm email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable #, :confirmable
            
   attr_accessible :email, :password, :password_confirmation, :default_time_zone, :remember_me, :roles
   
