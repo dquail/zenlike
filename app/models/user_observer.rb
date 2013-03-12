ac85234e87575fd1dbaf84544d51575ec4cddb4b
David Quail <quail.david@gmail.com>
2013-03-12 14:02:37 -0700

    Flash message update for bootstrap

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -3,12 +3,18 @@ class UserObserver < ActiveRecord::Observer
   
   def after_create(user)
     Rails.logger.debug "UserObserver.after_save"
+    
+    #default to have a user have a Free plan
     if (user.plan)
       Rails.logger.debug "Don't have to update the plan.  Already specified"
     else
       plan = Plan.find_by_name('Free')
       user.plan = plan
     end
+    
+    #default to have a user have a regular role
+    Rails.logger.debug "Setting the role"
+        
   end
   
 end
