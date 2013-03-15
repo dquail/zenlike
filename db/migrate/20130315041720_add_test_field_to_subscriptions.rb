0e6f159b9d6e2f697b42458ec9ba82c45547a7a0
David Quail <quail.david@gmail.com>
2013-03-14 21:23:27 -0700

    Last 4

--- db/migrate/20130315041720_add_test_field_to_subscriptions.rb
+++ db/migrate/20130315041720_add_test_field_to_subscriptions.rb
@@ -1,6 +1,5 @@
 class AddTestFieldToSubscriptions < ActiveRecord::Migration
   def change
-    remove_column :subscriptions, :last_4_digits
     add_column :subscriptions, :last_4_digits, :string, :limit => 4
   end
 end
