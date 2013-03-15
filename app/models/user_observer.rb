6e7aee17f2606f69db53beb49cca9e4e1a019694
David Quail <quail.david@gmail.com>
2013-03-15 11:57:36 -0700

    Create a free stripe subscription when account is created

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -8,8 +8,11 @@ class UserObserver < ActiveRecord::Observer
     if (user.plan)
       Rails.logger.debug "Don't have to update the plan.  Already specified"
     else
-      plan = Plan.find_by_name('Free')
-      user.plan = plan
+      sub = user.build_subscription
+      sub.plan = Plan.find_by_name('Free')
+      customer = Stripe::Customer.create(description: user.email, plan: sub.plan.stripe_id)
+      sub.stripe_customer_token = customer.id      
+      sub.save!
     end
     
     #default to have a user have a regular role
