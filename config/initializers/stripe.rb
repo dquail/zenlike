2487f2376b24ce3c298679f6b5546e1e6f37b83a
David Quail <quail.david@gmail.com>
2013-03-15 13:05:06 -0700

    Added error handling when strip sends us a customer we dont have

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,13 +4,19 @@ STRIPE_PUBLIC_KEY = "pk_test_bsLjkPKn3lNoDcVIpdYHvqeD"
 StripeEvent.setup do
   subscribe 'charge.failed' do |event|
     Rails.logger.info "Charge failed event from stripe"
-    subscription_from_event(event).charge_failed(event)
+    subscription = subscription_from_event(event)
+    if subscription
+      subscription.charge_failed(event)
+    end
     
   end
   
   subscribe 'charge.succeeded' do |event|
     Rails.logger.info "Charge succeeded event from stripe"    
-    subscription_from_event(event).charge_succeeded(event)
+    subscription = subscription_from_event(event)
+    if subscription
+      subscription.charge_succeeded(event)
+    end
   end
   
   subscribe 'invoiceitem.created' do |event|
@@ -33,6 +39,7 @@ def subscription_from_event(event)
     Rails.logger.info "found a subscription matching customer"
     return subscription
   else
+    Rails.logger.error "No customer found matching #{event.data.object.customer}"
     return nil
   end
 
