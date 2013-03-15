2fc7e692fffccdd76b6ab1af510ea6349a6be796
David Quail <quail.david@gmail.com>
2013-03-15 10:50:16 -0700

    Updated with more logging

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -12,12 +12,20 @@ StripeEvent.setup do
     Rails.logger.info "Charge succeeded event from stripe"    
     subscription_from_event(event).charge_succeeded(event)
   end
-
   
 end
 
 
 private 
 def subscription_from_event(event)
+  Rails.logger.info "customer data: #{event.data.object.customer}"
+  
   subscription = Subscription.find_by_stripe_customer_token(event.data.object.customer)
+  if (subscription)
+    Rails.logger.info "found a subscription matching customer"
+    return subscription
+  else
+    return nil
+  end
+
 end
